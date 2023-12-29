72547e584e1d0d3162932c6a03470ee9f7e1b66c
David M <136613722+realize-1337@users.noreply.github.com>
2023-12-29 22:27:42 +0100

    Update setup.bat

--- setup.bat
+++ setup.bat
@@ -1,14 +1,10 @@
 if not exist venv (
     python -m venv venv
-    echo Created a virtual environment
 )
 
 REM Activate the virtual environment and install requirements
 call .\venv\Scripts\activate.bat 
 call pip install -r requirements.txt
-
-REM Display a message and wait for user input
-set /p confirm=Press Enter to start building the software...
 
 REM Run setup.py
 call python setup.py
