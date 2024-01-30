9b6109f766d54c8fbaee87718649db8a037c77f2
David M <136613722+realize-1337@users.noreply.github.com>
2024-01-30 18:51:08 +0100

    added ID32 to ex2db

--- packages/Excel2DB.py
+++ packages/Excel2DB.py
@@ -8,6 +8,8 @@ class Excel2DB():
     def __init__(self, db:str, fullEx = None) -> None:
         self.db = sqlite3.connect(db)
         self.fullEx = fullEx
+        self.fullDf_n = pd.DataFrame(columns=['Name', '1H', '2H', 'VP', 'mean', 'pos', 'neg', 'std'])
+        self.fullDf_m = pd.DataFrame(columns=['Name', '1H', '2H', 'VP', 'mean', 'pos', 'neg', 'std'])
 
     def convert(self, file, sheet_name, table_name):
         df = pd.read_excel(file, sheet_name=sheet_name)
@@ -29,11 +31,32 @@ class Excel2DB():
             df.iloc[2] = ['Horizontal Position', 'Droplet Axial Velocity', 'Droplet Axial Velocity', 'Droplet Axial Velocity', 'Droplet Axial Velocity', 'Droplet Axial Velocity', 'Droplet Axial Velocity', 'Droplet Axial Velocity']
 
         df = df.set_index('x', drop=True)
-        df.to_sql(table_name, self.db, if_exists='replace')
+        # df.to_sql(table_name, self.db, if_exists='replace')
 
         if self.fullEx:
             with pd.ExcelWriter(self.fullEx, 'openpyxl', if_sheet_exists='replace', mode='a') as ex:
                 df.to_excel(ex, sheet_name=f'{sheet_name} {table_name}')
+    
+    def getIDs(self, file, sheet_name, table_name):
+        n = 0
+        m = 1
+        df = pd.read_excel(file, sheet_name=sheet_name, index_col=[0])
+        df['Name'] = [table_name, table_name]
+        order_raw = ['Name', '1H', '2H', 'VP', 'mean', 'pos', 'neg', 'std']
+        df = df[order_raw]
+        self.fullDf_n = pd.concat([self.fullDf_n, df.iloc[[n]]], ignore_index=True)
+        self.fullDf_m = pd.concat([self.fullDf_m, df.iloc[[m]]], ignore_index=True)
+
+    def saveIDs(self, file):
+        if os.path.exists(file):
+            with pd.ExcelWriter(file, 'openpyxl', if_sheet_exists='replace', mode='a') as ex:
+                self.fullDf_m.to_excel(ex, sheet_name='ID_32_m')
+                self.fullDf_n.to_excel(ex, sheet_name='ID_32_n')
+        else: 
+            with pd.ExcelWriter(file, 'openpyxl', mode='w') as ex:
+                self.fullDf_m.to_excel(ex, sheet_name='ID_32_m')
+                self.fullDf_n.to_excel(ex, sheet_name='ID_32_n')
+     
 
 
 if __name__ == '__main__':
@@ -52,8 +75,12 @@ if __name__ == '__main__':
         names.append(f'{l[0]} {l[1]} {l[2]}')
 
     db = r'C:\Users\david\Desktop\Test PDA\test.db'
+    iddb = r'C:\Users\david\Desktop\Test PDA\id.db'
     fullEx = r'C:\Users\david\Desktop\Test PDA\full.xlsx'
+    fullID = r'C:\Users\david\Desktop\Test PDA\fullID.xlsx'
+    fullID_save = r'C:\Users\david\Desktop\Test PDA\fullID_save.xlsx'
     ex2db = Excel2DB(db, fullEx)
+    id2ex = Excel2DB(iddb, fullID)
 
     folder = r'C:\Users\david\Desktop\Test PDA\All'
     if not os.path.exists(folder):
@@ -61,8 +88,11 @@ if __name__ == '__main__':
 
     for file, name in zip(found, names):
         try:
-            ex2db.convert(file, 'D32 Export', name)
-            ex2db.convert(file, 'v_z Export', name)
-            shutil.copy(file, os.path.join(folder, f'{name}.xlsx'))
+            id2ex.getIDs(file, 'ID_32', name)
+            # ex2db.convert(file, 'D32 Export', name)
+            # ex2db.convert(file, 'v_z Export', name)
+            # shutil.copy(file, os.path.join(folder, f'{name}.xlsx'))
         except: 
             pass
+
+        id2ex.saveIDs(fullID_save)
