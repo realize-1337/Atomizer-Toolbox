7a1ef27bafe81225f358ef74b7d426dcabf58bde
David M <136613722+realize-1337@users.noreply.github.com>
2024-02-21 09:31:24 +0100

    latest compile

--- version_sign/version.py
+++ version_sign/version.py
@@ -2,7 +2,7 @@ import pyinstaller_versionfile
 
 pyinstaller_versionfile.create_versionfile(
     output_file="version_sign/versionfile.txt",
-    version="1.6.8.2",
+    version="1.6.8.3",
     company_name="David Maerker",
     file_description="Utilities for atomizer research",
     internal_name="Atomizer Toolbox",
