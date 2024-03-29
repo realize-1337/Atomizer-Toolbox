f036a1ddaa183619f6cc688652443d262f8a16ef
David M <136613722+realize-1337@users.noreply.github.com>
2024-03-29 09:24:54 +0100

    bastlen_package

--- packages/basteln.py
+++ packages/basteln.py
@@ -2,8 +2,9 @@ import pandas as pd
 import numpy as np
 
 # calculate areas for PDA
-if __name__ == '__main__':
-    r_i = np.concatenate((np.arange(0, 22, 2), np.arange(25, 95, 5)))
+def PDA_Area(max=100):
+    if max < 20: raise ValueError
+    r_i = np.concatenate((np.arange(0, 22, 2), np.arange(25, max+5, 5)))
     delta_r = np.diff(r_i)
     areas = np.zeros_like(delta_r.astype(np.float32))
 
@@ -15,4 +16,13 @@ if __name__ == '__main__':
     
     df = pd.Series(areas[::-1])
     df.to_clipboard(decimal=',', header=False, index=False)
+
+def inverseClip(dec=','):
+    df = pd.read_clipboard(header=None, index_col=None)
+    arr = df.to_numpy()[::-1]
+    df = pd.DataFrame(arr)
+    df.to_clipboard(decimal=dec, header=False, index=False)
+
+if __name__ == '__main__':
+    inverseClip('.')
     pass
